54fdbec71700b747a0c3d26903ad304052c3064b
PaCtivsa <2421962762@qq.com>
2023-11-19 22:14:07 +0800

    init

--- template_test.py
+++ template_test.py
@@ -6,7 +6,9 @@ buildings = Buildings("buildings_explosives_building_chemical_plants",if_init=Tr
 m_pmg = Pmg("pmg_explosives_building_chemical_plants",if_init=True,Bindings=buildings)
 
 print(buildings.data)
+buildings.add("unique", "=", "yes")
 buildings.output("output/buildings.txt")
+
 m_pmg.output("output/pmg.txt")
 
 
